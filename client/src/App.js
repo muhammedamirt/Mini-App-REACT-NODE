2f7d4fe907332107ede4869d5574916e3732bb53
Muhammed Amir T <muhammedamirt@gmail.com>
2022-11-30 00:15:49 +0530

    Profile page aa

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Login from './components/login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Signup from './components/signup';
 import Home from './pages/Home/Home';
+import Profile from './pages/userProfile/Profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/signup' element={<Signup />} />
+        <Route exact path='/userprofile' element={<Profile />} />
       </Routes>
     </Router>
   )
---
+++ client/src/pages/userProfile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../../components/footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+import ProfileCompo from '../../components/profile/Profile'
+
+function Profile() {
+  return (
+    <>
+   <Navbar />
+   <ProfileCompo />
+   <Footer />
+    </>
+
+  )
+}
+
+export default Profile
